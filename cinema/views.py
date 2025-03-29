9a4cd8d91196d3d64923689ce043ba9f079e4391
Vadym Piddubnyi <leegrad@ukr.net>
2025-03-28 22:06:18 -0500

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -36,19 +36,17 @@ class GenreDetail(APIView):
 
     def put(self, request, pk: int) -> Response:
         serializer = GenreSerializer(self.get_object(pk=pk), data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def patch(self, request, pk: int) -> Response:
         serializer = GenreSerializer(
             self.get_object(pk=pk), data=request.data, partial=True
         )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, pk: int) -> Response:
         self.get_object(pk=pk).delete()
